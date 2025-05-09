01703f046d9674e7d87e426f917bc51db6dbbcda
chivkado <chivkado240@gmail.com>
2025-05-09 11:04:03 +0700

    first commit

---
+++ src/main/java/gic/i4/library_webapp_CHIVKado/LibraryWebappChivKadoApplication.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_CHIVKado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebappChivKadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWebappChivKadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gic/i4/library_webapp_CHIVKado/LibraryWebappChivKadoApplicationTests.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_CHIVKado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWebappChivKadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
